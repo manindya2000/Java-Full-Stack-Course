405c684ea366804c75423dcb2fce925c5fc1fb25
manindya2000 <manindya2000@gmail.com>
2025-02-11 01:29:25 +0530

    Yes please

---
+++ Classes/src/main/java/com/anindya/java/Classes/App.java
@@ -0,0 +1,13 @@
+package com.anindya.java.Classes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Classes/src/main/java/com/anindya/java/Classes/Car.java
@@ -0,0 +1,5 @@
+package com.anindya.java.Classes;
+
+public class Car {
+ String abc;
+}
